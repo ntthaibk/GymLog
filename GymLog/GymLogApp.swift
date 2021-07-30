d930d0ec8f84f396efd19b3b0ae8326065489cc6
Thai Nguyen <62538035+ntthaibk@users.noreply.github.com>
2021-07-30 21:15:01 +0700

    Initial Commit

---
+++ GymLog/GymLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymLogApp.swift
+//  GymLog
+//
+//  Created by LAP12842 on 30/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct GymLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
